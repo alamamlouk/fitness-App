8363db723ff607767aee43fae2f1059799b709b3
alaam <alaamamlouk09@gmail.com>
2024-02-02 22:28:21 +0100

    second commit

---
+++ app/src/main/java/com/example/fitnessapp/WorkOut.java
@@ -0,0 +1,45 @@
+package com.example.fitnessapp;
+
+public class WorkOut {
+    private int workoutImg, WorkOutTime,numberOfExercises;
+    private String WorkoutTile;
+
+    public WorkOut(int workoutImg, int workOutTime, int numberOfExercises, String workoutTile) {
+        this.workoutImg = workoutImg;
+        WorkOutTime = workOutTime;
+        this.numberOfExercises = numberOfExercises;
+        WorkoutTile = workoutTile;
+    }
+
+    public int getWorkoutImg() {
+        return workoutImg;
+    }
+
+    public void setWorkoutImg(int workoutImg) {
+        this.workoutImg = workoutImg;
+    }
+
+    public int getWorkOutTime() {
+        return WorkOutTime;
+    }
+
+    public void setWorkOutTime(int workOutTime) {
+        WorkOutTime = workOutTime;
+    }
+
+    public int getNumberOfExercises() {
+        return numberOfExercises;
+    }
+
+    public void setNumberOfExercises(int numberOfExercises) {
+        this.numberOfExercises = numberOfExercises;
+    }
+
+    public String getWorkoutTile() {
+        return WorkoutTile;
+    }
+
+    public void setWorkoutTile(String workoutTile) {
+        WorkoutTile = workoutTile;
+    }
+}
---
+++ app/src/main/java/com/example/fitnessapp/WorkOutAdapter.java
@@ -0,0 +1,67 @@
+package com.example.fitnessapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class WorkOutAdapter extends RecyclerView.Adapter<WorkOutAdapter.WorkOutViewHolder> {
+
+    ArrayList<WorkOut> workOuts;
+    public Context context;
+
+    public WorkOutAdapter(ArrayList<WorkOut> workOuts, Context context) {
+        this.workOuts = workOuts;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public WorkOutAdapter.WorkOutViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.workout_item,parent,false);
+        return new WorkOutViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WorkOutAdapter.WorkOutViewHolder holder, int position) {
+        WorkOut workOut=workOuts.get(position);
+        holder.workoutImage.setImageResource(workOut.getWorkoutImg());
+        holder.workOutTime.setText(String.valueOf(workOut.getWorkOutTime()));
+        holder.numberOfExercises.setText(String.valueOf(workOut.getNumberOfExercises()));
+        holder.workOutName.setText(workOut.getWorkoutTile());
+        holder.itemView.setBackgroundColor(getBackgroundColor(position));
+
+    }
+    private int getBackgroundColor(int position) {
+        if (position % 2 == 0) {
+            return ContextCompat.getColor(context, R.color.orange);
+        }
+        return ContextCompat.getColor(context, R.color.darkgreen);
+    }
+
+    @Override
+    public int getItemCount() {
+        return workOuts.size();
+    }
+
+    public class WorkOutViewHolder extends RecyclerView.ViewHolder{
+        public ImageView workoutImage;
+        public TextView workOutName,numberOfExercises,workOutTime;
+
+        public WorkOutViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.workoutImage=itemView.findViewById(R.id.workOutImage);
+            this.workOutName=itemView.findViewById(R.id.workOutName);
+            this.numberOfExercises=itemView.findViewById(R.id.nbExercises);
+            this.workOutTime=itemView.findViewById(R.id.timeToFinish);
+        }
+    }
+}
